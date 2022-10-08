532351f88bc345b77d7cd8301a64fb00882e8036
nth-commit <mfrywhite@gmail.com>
2022-10-09 07:56:49 +1300

    Add watcher for tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
+import { HardhatUserConfig } from 'hardhat/config'
+import 'hardhat-watcher'
 
 const config: HardhatUserConfig = {
   solidity: '0.8.17',
+  watcher: {
+    test: {
+      tasks: ['test'],
+      files: ['./contracts', './test'],
+      clearOnStart: true,
+    },
+  },
 }
 
 export default config
